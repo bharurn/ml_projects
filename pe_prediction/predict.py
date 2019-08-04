7c81879c8badfa7eb79d1a1fef1347cd0755bdd9
Bharath Raghavan <52472738+bharurn@users.noreply.github.com>
2019-08-04 06:06:15 -0400

    Updated to use chemml package

--- pe_prediction/predict.py
+++ pe_prediction/predict.py
@@ -1,36 +1,10 @@
 import pybel
-import numpy as np
+import chemml
 from sklearn.externals import joblib
-import pandas as pd
 
-max_atoms = 30
+chemml.max_atoms = 30
 
-def getdist(a1, a2):
-    
-    x1 = a1.coords[0]
-    y1 = a1.coords[1]
-    z1 = a1.coords[2]
-    x2 = a2.coords[0]
-    y2 = a2.coords[1]
-    z2 = a2.coords[2]
-    return ((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)**0.5
-
-def CoulombMatrix(mol):
-    cm = np.zeros((max_atoms, max_atoms))
-    mag = []
-    for i in mol:
-        c = np.zeros(max_atoms)
-        for j in mol:
-            if i.idx == j.idx:
-                c[j.idx-1] = (0.5*j.atomicnum**2.4)
-            elif i.idx != j.idx:
-                c[j.idx-1] = i.atomicnum*j.atomicnum/getdist(i, j)
-        mag.append(np.linalg.norm(c))
-        cm[i.idx-1] = c
-    
-    return np.linalg.eigvals(cm)
-
-data = pd.read_csv("test_pe.csv") #load test data
+data = chemml.pd.read_csv("test_pe.csv") #load test data
 
 C = []
 
@@ -44,7 +18,7 @@ for mol_name in mols:
         print ("Error in loading molecule: " + str(mol_name) + ". Skipping...")
         continue
     
-    C.append(CoulombMatrix(m))
+    C.append(chemml.CoulombMatrixEig(m))
     
 kr = joblib.load('pe.model') #load model
 
