89d0710d74a27319c57b691e19a15acf24751243
Bharath Raghavan <52472738+bharurn@users.noreply.github.com>
2019-08-04 04:59:02 -0400

    Changed ml algol to DNN using tensorflow
    Also changed to custom chemml package

--- dipole_prediction/train.py
+++ dipole_prediction/train.py
@@ -1,75 +1,44 @@
-import pybel
-import numpy as np
-import pandas as pd
-from sklearn.kernel_ridge import KernelRidge
-from sklearn.externals import joblib
+import chemml
 
-max_atoms = 30
+chemml.max_atoms = 30
 
-def getdist(a1, a2):
-    
-    x1 = a1.coords[0]
-    y1 = a1.coords[1]
-    z1 = a1.coords[2]
-    x2 = a2.coords[0]
-    y2 = a2.coords[1]
-    z2 = a2.coords[2]
-    return ((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)**0.5
-
-def CoulombMatrix(mol):
-    cm = np.zeros((max_atoms, max_atoms))
-    mag = []
-    for i in mol:
-        c = np.zeros(max_atoms)
-        for j in mol:
-            if i.idx == j.idx:
-                c[j.idx-1] = (0.5*j.atomicnum**2.4)
-            elif i.idx != j.idx:
-                c[j.idx-1] = i.atomicnum*j.atomicnum/getdist(i, j)
-        mag.append(np.linalg.norm(c))
-        cm[i.idx-1] = c
-    
-    mag.extend([0]*(max_atoms-len(mol.atoms)))
-        
-    sorter = pd.DataFrame(list(zip(mag, cm)), columns=['mag', 'cm'])
-        
-    sorter = sorter.sort_values('mag', ascending=False)
-    
-    s = sorter['cm'].to_numpy()
-    
-    scm = np.array([i for i in s])
-    
-    return scm
-
-m = next(pybel.readfile("xyz", "test.xyz"))
-CoulombMatrix(m)
-
-data = pd.read_csv("dipole_moments.csv", nrows=5000) #read data
+data = chemml.pd.read_csv("dipole_moments.csv", nrows=5000) #read data
 
 C = []
 
 mols = list(dict.fromkeys(data['molecule_name'])) #get unique molecule names
 
 for mol_name in mols:
-    m = next(pybel.readfile("xyz", "structures/" + mol_name +".xyz")) #load the molecule
+    m = next(chemml.pb.readfile("xyz", "structures/" + mol_name +".xyz")) #load the molecule
     
     if m is None: #check if the structure is loaded
         data = data[data.molecule_name!=mol_name] #if there is mismatch, delete this entry from dataframe
         print ("Error in loading molecule: " + str(mol_name) + ". Skipping...")
         continue
     
-    C.append(CoulombMatrix(m).flatten())
-        
-kr = KernelRidge(alpha=1.0)
+    C.append(chemml.CoulombMatrix(m))
+    
+X = chemml.np.array(C)
+            
+Y = chemml.np.column_stack((data['X'].to_numpy(),data['Y'].to_numpy(), data['Z'].to_numpy()))
 
-kr.fit(C, data['X'].to_numpy())
+feature_columns = [chemml.tf.feature_column.numeric_column('x', shape=X.shape[1:])]
 
-joblib.dump(kr, 'dipoleX.model', compress=9)
+estimator = chemml.tf.estimator.DNNRegressor(feature_columns=feature_columns,
+     model_dir='train_dipole',    
+     hidden_units=[500, 300],   
+     label_dimension = 3,
+     optimizer=chemml.tf.train.ProximalAdagradOptimizer(      
+          learning_rate=0.1,      
+          l1_regularization_strength=0.001    
+      )
+)
 
-kr.fit(C, data['Y'].to_numpy())
+train_input = chemml.tf.estimator.inputs.numpy_input_fn(   
+           x={"x": X},    
+           y=Y,    
+           #batch_size=128,    
+           shuffle=False,    
+           num_epochs=None)
 
-joblib.dump(kr, 'dipoleY.model', compress=9)
-
-kr.fit(C, data['Z'].to_numpy())
-
-joblib.dump(kr, 'dipoleZ.model', compress=9)
+estimator.train(input_fn = train_input,steps=5000)
