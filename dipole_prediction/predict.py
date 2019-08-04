11b57919d4bcd1a89b65a7bc0fa2a3f0baf01880
Bharath Raghavan <52472738+bharurn@users.noreply.github.com>
2019-08-04 05:00:04 -0400

    Updated file for tensorflow
    Also workflow changed to chemml package

--- dipole_prediction/predict.py
+++ dipole_prediction/predict.py
@@ -1,67 +1,38 @@
-import pybel
-import numpy as np
-from sklearn.externals import joblib
-import pandas as pd
+import chemml
 
-max_atoms = 30
+chemml.max_atoms = 30
 
-def getdist(a1, a2):
-    
-    x1 = a1.coords[0]
-    y1 = a1.coords[1]
-    z1 = a1.coords[2]
-    x2 = a2.coords[0]
-    y2 = a2.coords[1]
-    z2 = a2.coords[2]
-    return ((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)**0.5
-
-def CoulombMatrix(mol):
-    cm = np.zeros((max_atoms, max_atoms))
-    mag = []
-    for i in mol:
-        c = np.zeros(max_atoms)
-        for j in mol:
-            if i.idx == j.idx:
-                c[j.idx-1] = (0.5*j.atomicnum**2.4)
-            elif i.idx != j.idx:
-                c[j.idx-1] = i.atomicnum*j.atomicnum/getdist(i, j)
-        mag.append(np.linalg.norm(c))
-        cm[i.idx-1] = c
-    
-    mag.extend([0]*(max_atoms-len(mol.atoms)))
-        
-    sorter = pd.DataFrame(list(zip(mag, cm)), columns=['mag', 'cm'])
-        
-    sorter = sorter.sort_values('mag', ascending=False)
-    
-    s = sorter['cm'].to_numpy()
-    
-    scm = np.array([i for i in s])
-    
-    return scm
-
-data = pd.read_csv("test_dipoles.csv") #load test data
+data = chemml.pd.read_csv("test_dipoles.csv") #load test data
 
 C = []
 
 mols = list(dict.fromkeys(data['molecule_name'])) #get unique molecule names
 
 for mol_name in mols:
-    m = next(pybel.readfile("xyz", "structures/" + mol_name +".xyz"))
+    m = next(chemml.pb.readfile("xyz", "structures/" + mol_name +".xyz"))
     
     if m is None: #check if the structure is loaded
         data = data[data.molecule_name!=mol_name] #if there is mismatch, delete this entry from dataframe
         print ("Error in loading molecule: " + str(mol_name) + ". Skipping...")
         continue
     
-    C.append(CoulombMatrix(m).flatten())
+    C.append(chemml.CoulombMatrix(m))
     
-krX = joblib.load('dipoleX.model') #load model
-krY = joblib.load('dipoleY.model') #load model
-krZ = joblib.load('dipoleZ.model') #load model
+X = chemml.np.array(C)
 
-#save predicted values to csv
-data['predicted-X'] = krX.predict(C)
-data['predicted-Y'] = krY.predict(C)
-data['predicted-Z'] = krZ.predict(C)
-data.to_csv(r"predicted_dipole.csv")
+feature_columns = [chemml.tf.feature_column.numeric_column('x', shape=X.shape[1:])]
+
+test = chemml.tf.estimator.DNNRegressor(feature_columns=feature_columns,
+     model_dir='train_dipole',    
+     hidden_units=[500, 300],
+     label_dimension = 3
+)
+     
+x_input = chemml.tf.estimator.inputs.numpy_input_fn(x={"x": X}, shuffle=False)
+     
+y = test.predict(input_fn=x_input)
+
+for i in y:
+    print (i)
+
+#data.to_csv(r"predicted_X.csv")
